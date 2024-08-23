5ac87ca7d77806b942fc089e1857927af72ef720
Paolo Magri <150929375+polmagri@users.noreply.github.com>
2024-08-23 15:18:37 +0200

    download_dataset.py

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -5,10 +5,10 @@ import gdown
 google_drive_link = 'https://drive.google.com/drive/folders/1_3JckYWL6bLGh8cu_JtG2LzMEAdoCGat?usp=sharing'
 
 # Specifica il percorso della cartella dove vuoi salvare il dataset
-download_path = '/dataset/'
+download_path = '~/Downloads/dataset/'
 
 # Crea la cartella se non esiste già
 os.makedirs(download_path, exist_ok=True)
 
 # Scarica il dataset
-gdown.download(google_drive_link, os.path.join(download_path, 'nome_del_file.ext'), quiet=False)
+gdown.download(google_drive_link, os.path.join(download_path, 'dataset.zip'), quiet=False)
