5e6fa77de8bc1009c99f1ce203f2fbd9bd170e70
Paolo Magri <150929375+polmagri@users.noreply.github.com>
2024-09-11 17:41:39 +0200

    Update download_dataset.py

--- people_det_3d/src/people_det_3d/download_dataset.py
+++ people_det_3d/src/people_det_3d/download_dataset.py
@@ -1,14 +1,13 @@
 import os
 import gdown
 
-# Inserisci qui il link di Google Drive
 google_drive_link = 'https://drive.google.com/drive/folders/1_3JckYWL6bLGh8cu_JtG2LzMEAdoCGat?usp=sharing'
 
-# Specifica il percorso della cartella dove vuoi salvare il dataset
+
 download_path = '~/Downloads/dataset/'
 
-# Crea la cartella se non esiste già
+
 os.makedirs(download_path, exist_ok=True)
 
-# Scarica il dataset
+# Download dataset
 gdown.download(google_drive_link, os.path.join(download_path, 'dataset.zip'), quiet=False)
